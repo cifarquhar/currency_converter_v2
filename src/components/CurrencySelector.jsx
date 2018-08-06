2fe885781eb2933f08af8947836952cc6ebbc097
cifarquhar <colin.farquhar@codeclan.com>
2018-08-06 15:58:17 +0100

    add CurrencySelector and add to container

---
+++ src/components/CurrencySelector.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class CurrencySelector extends Component{
+
+render(){
+    return <p>Selector</p>
+}
+
+}
+
+export default CurrencySelector;
--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,13 +1,19 @@
 import React, {Component} from "react";
+import CurrencySelector from "../components/CurrencySelector";
 
 class MainContainer extends Component{
-
-render(){
-    return(
-        <h1>Content in here</h1>
-    )
-}
-
+	
+	render(){
+		const p = this.props;
+		return(
+			<div>
+				<CurrencySelector rates={p.rates} />
+				<CurrencySelector rates={p.rates} />
+			</div>
+			
+		)
+	}
+	
 }
 
 export default MainContainer;
