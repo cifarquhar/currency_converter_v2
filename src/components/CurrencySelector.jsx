3c3c60b43d941f236cde6355f550f347f816c342
cifarquhar <colin.farquhar@codeclan.com>
2018-08-06 16:20:15 +0100

    add currency selector component

--- src/components/CurrencySelector.jsx
+++ src/components/CurrencySelector.jsx
@@ -1,9 +1,30 @@
 import React, {Component} from "react";
+import {FormGroup, FormControl} from "react-bootstrap";
 
 class CurrencySelector extends Component{
 
+constructor(props){
+    super(props)
+}
+
 render(){
-    return <p>Selector</p>
+
+    // console.log(this.props.rates.keys(obj))
+
+    let options = []
+
+    // this.props.rates.keys.forEach(key => {
+    //     options.push(<option value={key}>key</option>)
+    // });
+
+    return (
+        <p>content</p>
+        // <FormGroup>
+        //     <FormControl>
+        //         {options}
+        //     </FormControl>
+        // </FormGroup>
+    )
 }
 
 }
