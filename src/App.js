5bf74f5de915255237985a0e8cc60ae235ff3244
cifarquhar <colin.farquhar@codeclan.com>
2018-08-06 15:44:01 +0100

    refactor App to include API call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import ReactDOM from "react-dom";
 import './App.css';
 import MainContainer from "./containers/MainContainer";
 
 class App extends Component {
+
+  componentDidMount(){
+  const key = "88c0871b1c32feaf2082dc168d45b14e";
+  const request = new XMLHttpRequest();
+  request.open("GET", "http://data.fixer.io/api/latest?access_key=" + key);
+  request.onload = function () {
+    if (request.status === 200) {
+      const jsonString = request.responseText;
+      const data = JSON.parse(jsonString);
+      console.log(data)
+      ReactDOM.render(
+        <MainContainer rates={data} />,
+        document.getElementById('root')
+      );
+    }
+  };
+  request.send();
+}
+
   render() {
-    return (
-      <MainContainer />
-    );
+    return null;
   }
 }
 
