1df539846b4503b9ef91cfc4dc037b35409683ce
cifarquhar <colin.farquhar@codeclan.com>
2018-08-06 15:00:38 +0100

    add main container

---
+++ src/containers/MainContainer.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class MainContainer extends Component{
+
+render(){
+    return(
+        <h1>Content in here</h1>
+    )
+}
+
+}
+
+export default MainContainer;
