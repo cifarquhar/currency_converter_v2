78766e7243f14af8792d41cb626b356ed5e04a52
cifarquhar <colin.farquhar@codeclan.com>
2018-08-07 10:28:16 +0100

    add constructor to container

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -3,15 +3,23 @@ import CurrencySelector from "../components/CurrencySelector";
 import CurrencyValue from "../components/CurrencyValue";
 
 class MainContainer extends Component{
+
+	constructor(props){
+		super(props);
+		this.leftCurrency = null;
+		this.rightCurrency = null;
+		this.leftValue = 0;
+		this.rightValue = 0;
+	}
 	
 	render(){
 		const p = this.props;
 		return(
 			<div>
 				<CurrencySelector rates={p.rates} />
-				<CurrencyValue />
+				<CurrencyValue value={this.leftValue}/>
 				<CurrencySelector rates={p.rates} />
-				<CurrencyValue />
+				<CurrencyValue value={this.rightValue}/>
 			</div>
 			
 		)
