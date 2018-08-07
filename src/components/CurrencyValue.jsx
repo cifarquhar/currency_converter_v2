9ed169fc7350116973c3c03e9fd526715e3de059
cifarquhar <colin.farquhar@codeclan.com>
2018-08-07 10:17:04 +0100

    create CurrencyValue component

---
+++ src/components/CurrencyValue.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class CurrencyValue extends Component{
+
+    render(){
+        return(
+            <input placeholder={value}/>
+        )
+    }
+
+}
+
+export default CurrencyValue;
