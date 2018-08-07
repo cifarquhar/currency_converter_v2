df9b8089e75f11b97fda6ac7d20441f6a011d606
cifarquhar <colin.farquhar@codeclan.com>
2018-08-07 14:45:46 +0100

    add InputService for pub-sub

---
+++ src/models/InputService.js
@@ -0,0 +1,18 @@
+import { Subject } from "rxjs";
+
+class InputService {
+
+    constructor() {
+        this.subject = new Subject();
+    }
+
+    emit(value) {
+        this.subject.next(value);
+    }
+
+    values() {
+        return this.subject.asObservable();
+    }
+}
+
+export default InputService
