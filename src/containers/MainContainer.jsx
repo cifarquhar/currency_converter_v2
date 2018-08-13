c68c10211c9f495f0bf1918d63fda10b0b8a0331
cifarquhar <colin.farquhar@codeclan.com>
2018-08-13 16:09:45 +0100

    add bootstrap styling to container

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { Panel } from "react-bootstrap";
 import CurrencySelector from "../components/CurrencySelector";
 import CurrencyValue from "../components/CurrencyValue";
 import SelectorService from "../models/SelectorService";
@@ -31,10 +32,20 @@ class MainContainer extends Component{
 		const p = this.props;
 		return(
 			<div>
-				<CurrencySelector rates={p.rates} selectorService={leftSelectorService}/>
-				<CurrencyValue side={"left"} rates={p.rates} value={this.leftValue} selectorService={leftSelectorService} outputService={leftInputService} inputService={rightInputService}/>
-				<CurrencySelector rates={p.rates} selectorService={rightSelectorService}/>
-				<CurrencyValue side={"right"} rates={p.rates} value={this.rightValue} selectorService={rightSelectorService} outputService={rightInputService} inputService={leftInputService}/>
+				<Panel>
+					<Panel.Heading>Title</Panel.Heading>
+					<Panel.Body>
+						<CurrencySelector rates={p.rates} selectorService={leftSelectorService}/>
+						<CurrencyValue side={"left"} rates={p.rates} value={this.leftValue} selectorService={leftSelectorService} outputService={leftInputService} inputService={rightInputService}/>
+					</Panel.Body>
+				</Panel>
+				<Panel>
+					<Panel.Heading>Title</Panel.Heading>
+					<Panel.Body>
+						<CurrencySelector rates={p.rates} selectorService={rightSelectorService}/>
+						<CurrencyValue side={"right"} rates={p.rates} value={this.rightValue} selectorService={rightSelectorService} outputService={rightInputService} inputService={leftInputService}/>
+					</Panel.Body>
+				</Panel>
 			</div>
 			
 		)
